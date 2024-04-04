72121bddeffaa1719215b963c786c6c0745fd789
Santiago San Martin <ssanmartin@buk.cl>
2024-04-04 17:53:33 -0300

    add template detail book template

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,21 +3,37 @@
 namespace App\Controller;
 
 use App\Repository\BookRepository;
+use App\Service\Book\GetBook;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations\{Delete, Get, Post, Put};
 
 class BookController extends AbstractController
 {
-    #[Route('/book', name: 'app_book')]
-    public function index(
-        BookRepository $bookRepository,
-    ): Response
+    public function __construct(private BookRepository $bookRepository)
+    {
+
+    }
+
+    #[Get('/book', name: 'app_book')]
+    public function index(): Response
     {
 
         return $this->render('book/index.html.twig', [
             'controller_name' => 'BookController',
-            'books' =>  $bookRepository->findAll(),
+            'books' =>  $this->bookRepository->findAll(),
         ]);
     }
+
+    #[Get('/book/{id}', name: 'detail_book')]
+    public function details(string $id, GetBook $getBook): Response
+    {
+        $book = $getBook->__invoke($id);
+
+        return $this->render('book/details.html.twig', [
+            'book' => $book,
+        ]);
+    }
+
 }
