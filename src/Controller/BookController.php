6c70ad3482b277a56f6eaf2f65679af504dad1c8
Santiago San Martin <ssanmartin@buk.cl>
2024-04-04 17:19:37 -0300

    add table with books

---
+++ src/Controller/BookController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BookRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookController extends AbstractController
+{
+    #[Route('/book', name: 'app_book')]
+    public function index(
+        BookRepository $bookRepository,
+    ): Response
+    {
+
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController',
+            'books' =>  $bookRepository->findAll(),
+        ]);
+    }
+}
